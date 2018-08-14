6449939c942758d24dba5d5280acb48aaa61f015
lishion <544670411@QQ.com>
2018-08-14 10:48:46 +0800

    修复:打开https链接错误的问题

--- src/main/index.js
+++ src/main/index.js
@@ -99,5 +99,6 @@ const {shell} = require('electron')
 //当用户点击了内容的中的连接，需要主动调用浏览器，否则会自动打开一个electron窗口
 //事件来自于 `info-card.vue`
 ipcMain.on('click-content-url',(event,target)=>{
-  shell.openExternal(target.startsWith("http://") ? target : `http://${target}`)
+  console.info(target.startsWith("http://") ? target : `http://${target}`)
+  shell.openExternal(target.startsWith("http://")||target.startsWith("https://") ? target : `http://${target}`)
 })
