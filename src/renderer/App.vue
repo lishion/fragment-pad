51341424d2c667030dd976622f89c6758ff3f82f
lishion <544670411@qq.com>
2019-01-18 10:49:32 +0800

    修改: 取消搜索框固定位置，随内容一起滚动

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,92 +1,104 @@
 <template>
-  <div id="app" :style="{backgroundImage: 'url(' + bg_url + ')',height:clientHeight,backgroundAttachment:attachment,backgroundSize:size,backgroundPosition:position}">
+  <div
+    id="app"
+    :style="{backgroundImage: 'url(' + bg_url + ')',height:clientHeight,backgroundAttachment:attachment,backgroundSize:size,backgroundPosition:position}"
+  >
     <div>
-        <el-row>
-          <el-col :span="22"><div style="height:50px;-webkit-app-region: drag"></div></el-col>
-          <el-col :span="2">
-            <el-popover
-                placement="bottom"
-                trigger="click">
-                <setting @on-bg-change="changeBackground"></setting>
-                <el-button slot="reference" type="text" icon="el-icon-setting"></el-button>
-            </el-popover>
-            <!--最小化按钮-->
-            <el-button type="text" icon="el-icon-minus" style="margin-left:1em" @click="minimize"></el-button>
-          </el-col>
-       </el-row>
+      <el-row>
+        <el-col :span="22">
+          <div style="height:50px;-webkit-app-region: drag"></div>
+        </el-col>
+        <el-col :span="2">
+          <el-popover placement="bottom" trigger="click">
+            <setting @on-bg-change="changeBackground"></setting>
+            <el-button slot="reference" type="text" icon="el-icon-setting"></el-button>
+          </el-popover>
+          <!--最小化按钮-->
+          <el-button type="text" icon="el-icon-minus" style="margin-left:1em" @click="minimize"></el-button>
+        </el-col>
+      </el-row>
     </div>
     <router-view style="-webkit-app-region: no-drag"></router-view>
   </div>
 </template>
 
 <style>
-  .hljs{
-    background: none
-  }
+.hljs {
+  background: none;
+}
 </style>
 
 
 <script>
-  import setting from './components/setting'
-  import {UserSetting} from './assets/js/utils'
-  let userSetting = UserSetting.getInstance()
-  import Bus from './assets/js/bus'
-  export default {
-    name: 'fragment-pad',
-    components:{setting},
-    data:function(){
-      return {
-        bg_url:require('./assets/bg/'+userSetting.getBackgroundImageOr("./assets/bg/watermelon.jpg")),
-        clientHeight:"600px",
-        attachment:"fixed",
-        repeat:"no-repeat",
-        position:"center",
-        size:"cover"
-        }
-    },
-    mounted() {
+import setting from "./components/setting";
+import { UserSetting } from "./assets/js/utils";
+let userSetting = UserSetting.getInstance();
+import Bus from "./assets/js/bus";
+export default {
+  name: "fragment-pad",
+  components: { setting },
+  data: function() {
+    return {
+      bg_url: require("./assets/bg/" +
+        userSetting.getBackgroundImageOr("./assets/bg/watermelon.jpg")),
+      clientHeight: "600px",
+      attachment: "fixed",
+      repeat: "no-repeat",
+      position: "center",
+      size: "cover"
+    };
+  },
+  mounted() {
+    //监听 resize 与 onscroll 事件，动态调整div大小，避免背景图空白
+    const that = this;
+    var rollHeight = 0;
+    var clientHeight = 0;
     
-      //监听 resize 与 onscroll 事件，动态调整div大小，避免背景图空白
-      const that = this;
-      var rollHeight = 0
-      var clientHeight = 0
-      window.onresize = function temp() {
-          clientHeight = document.documentElement.clientHeight
-          that.clientHeight = `${document.documentElement.clientHeight + window.scrollY}px`;
-      };
-      window.onscroll = function scroll(){
-        let rollHeigth = window.scrollY
-        that.clientHeight = `${document.documentElement.clientHeight + window.scrollY}px`
-      };  
-      //进入/退出搜索时，滚动条设置为0，避免继承上个页面的滚动条
-      Bus.$on("on-search",()=>{ 
-        that.clientHeight = "600px"
-      })
-      Bus.$on("cancel-search",()=>{
-        that.clientHeight = "600px"
-      })
-      Bus.$on("on-bg-set",(bg)=>{
-        this.bg_url = require(`./assets/bg/${bg}`)
-        userSetting.setBackgroundImage(bg)
-      })
-    },
-    methods:{
-      changeBackground(bg){
-        this.bg_url = require(`./assets/bg/${bg}`)
-        userSetting.setBackgroundImage(bg)
-      },
-      minimize(){
-        var ipc =  require('electron').ipcRenderer
-        ipc.send('minimize')
-      }
-    }
+    // 窗体目前不允许 resize 所以代码暂时不起作用
+    // window.onresize = () => {
+    //   clientHeight = document.documentElement.clientHeight;
+    //   that.clientHeight = `${document.documentElement.clientHeight + window.scrollY}px`;
+    // };
 
+    window.onscroll = () => {
+      let rollHeigth = window.scrollY;
+      that.clientHeight = `${document.documentElement.clientHeight + window.scrollY}px`;
+    };
+    
+    //进入/退出搜索时，滚动条设置为0，避免继承上个页面的滚动条
+    Bus.$on("on-search", () => {
+      that.clientHeight = "600px";
+    });
+    Bus.$on("cancel-search", () => {
+      that.clientHeight = "600px";
+    });
+
+    // 更换壁纸
+    Bus.$on("on-bg-set", bg => {
+      this.bg_url = require(`./assets/bg/${bg}`);
+      userSetting.setBackgroundImage(bg);
+    });
+
+  },
+  methods: {
+    changeBackground(bg) {
+      this.bg_url = require(`./assets/bg/${bg}`);
+      userSetting.setBackgroundImage(bg);
+    },
+    minimize() {
+      var ipc = require("electron").ipcRenderer;
+      ipc.send("minimize");
+    }
   }
+};
 </script>
 
 <style>
-  *{border: 0;margin: 0%}
-    ::-webkit-scrollbar {
-            display: none;
-    }
+* {
+  border: 0;
+  margin: 0%;
+}
+::-webkit-scrollbar {
+  display: none;
+}
 </style>
--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -1,110 +1,107 @@
 <template>
-    <el-container>
-        <!-- header 包括搜索以及添加按钮 -->
-        <el-header id="header"> 
-            <el-row type="flex" justify="center">
-                <el-col :span="10" style="width:60%">
-                    <div 
-                        class="grid-content" 
-                        @keydown.enter="search(keywords)" 
-                    >
-                        <el-input
-                            v-model="keywords"
-                            placeholder="输入以搜索"
-                            :prefix-icon="icon" 
-                            :disabled="editingItemKey!=null"
-                        >
-                        </el-input>
-                    </div>
-                </el-col>
-                <el-col :span="4" :push="3">  
-                    <div class="grid-content" >
-                        <el-button slot="reference" icon="el-icon-plus" type="text"  @click="addEmptyCard" :disabled="searchMode||editingItemKey!=null"></el-button>
-                    </div>
-                </el-col>
-            </el-row>
-        </el-header>
-        
-        <el-main style="margin-top: 60px" id="main">
-                
-                <div  
-                    v-for="item in items"  
-                    :key="item.key" 
-                    class="card"
-                >
-                    <!--如果是编辑模式，则显示编辑界面-->
-                    <addcard 
-                        v-if="edit_able[item.key]" 
-                        :item=item
-                        @on-save="save"
-                        id="addcard"
-                    >
-                    </addcard>
-                    
-                    <!--否则显示普通界面-->
-                    <!--searchMode||editingItemKey 用于控制删除按钮是否可用-->
-                    <infocard   
-                        v-else
-                        id="infocard"
-                        @on-delete="onDeleteSuccess" 
-                        @on-modify="modify(item)"
-                        :itemKey="item.key"
-                        :searchMode="searchMode"
-                        :editingItemKey="editingItemKey"
-                        :style="{backgroundColor: 'rgba(255, 255, 255,' + alpha + ')'}"   
-                    >   
-                        <template slot="title">
-                            <div v-html="item.value.rendered_title" v-if="item.value.rendered_title"></div>
-                            <div v-else>{{item.value.title}}</div>
-                        </template>
-                        <template slot="content">
-                            <div v-html="item.value.marked_content"></div>
-                        </template>
-                    </infocard>
-                </div>
-        </el-main>
+  <el-container>
+    <!-- header 包括搜索以及添加按钮 -->
+    <el-header id="header" :style="{display:showSearch}">
+      <el-row type="flex" justify="center">
+        <el-col :span="10" style="width:60%">
+          <div class="grid-content" @keydown.enter="search(keywords)">
+            <el-input
+              v-model="keywords"
+              placeholder="输入以搜索"
+              :prefix-icon="icon"
+              :disabled="editingItemKey!=null"
+            ></el-input>
+          </div>
+        </el-col>
+        <el-col :span="4" :push="3">
+          <div class="grid-content">
+            <el-button
+              slot="reference"
+              icon="el-icon-plus"
+              type="text"
+              @click="addEmptyCard"
+              :disabled="searchMode||editingItemKey!=null"
+            ></el-button>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
 
-        <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy"></div>   
-    </el-container>
+    <el-main style="margin-top: 60px" id="main">
+      <div v-for="item in items" :key="item.key" class="card">
+        <!--如果是编辑模式，则显示编辑界面-->
+        <addcard v-if="edit_able[item.key]" :item="item" @on-save="save" id="addcard"></addcard>
+
+        <!--否则显示普通界面-->
+        <!--searchMode||editingItemKey 用于控制删除按钮是否可用-->
+        <infocard
+          v-else
+          id="infocard"
+          @on-delete="onDeleteSuccess"
+          @on-modify="modify(item)"
+          :itemKey="item.key"
+          :searchMode="searchMode"
+          :editingItemKey="editingItemKey"
+          :style="{backgroundColor: 'rgba(255, 255, 255,' + alpha + ')'}"
+        >
+          <template slot="title">
+            <div v-html="item.value.rendered_title" v-if="item.value.rendered_title"></div>
+            <div v-else>{{item.value.title}}</div>
+          </template>
+          <template slot="content">
+            <div v-html="item.value.marked_content"></div>
+          </template>
+        </infocard>
+      </div>
+    </el-main>
+
+    <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy"></div>
+  </el-container>
 </template>
 
 <style >
-    #header{
-        width: 100%;
-        height: 60px;
-        position: fixed;
-        left: 0;
-        top: 50px;     
-    }
-    #header input{
-        background: transparent;
-        border-top:none;
-        border-left:none;
-        border-right:none;   
-        outline: none;
-        border-radius:0px; 
-    }
-   
-    #header input:focus{
-        border-radius: 4px; 
-        background:rgba(255, 255, 255,0.8); 
-        border: none;
-    }
-    #addcard  .el-input__inner{border: 0px ;border-bottom:1px solid rgb(200, 200, 159);border-radius:0 }
-    #addcard  .el-textarea__inner{border: 0px;border-bottom:1px solid rgb(200, 200, 159);border-radius:0}
+#header {
+  width: 100%;
+  height: 60px;
+  left: 0;
+  top: 50px;
+}
+#header input {
+  background: transparent;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  outline: none;
+  border-radius: 0px;
+}
 
+#header input:focus {
+  border-radius: 4px;
+  background: rgba(255, 255, 255, 0.8);
+  border: none;
+}
+
+#addcard .el-input__inner {
+  border: 0px;
+  border-bottom: 1px solid rgb(200, 200, 159);
+  border-radius: 0;
+}
+#addcard .el-textarea__inner {
+  border: 0px;
+  border-bottom: 1px solid rgb(200, 200, 159);
+  border-radius: 0;
+}
 </style>
 
 <script>
-
 import infocard from "./info-card";
 import addcard from "./add-card";
 import infiniteScroll from "vue-infinite-scroll";
-import {LevelDb,MessageBox,UserSetting} from '../assets/js/utils'
-import Bus from '../assets/js/bus'
+import { LevelDb, MessageBox, UserSetting } from "../assets/js/utils";
+import Bus from "../assets/js/bus";
 let leveldb = LevelDb.getInstance();
-let setting = UserSetting.getInstance()
-let ipcRender = require('electron').ipcRenderer
+let setting = UserSetting.getInstance();
+let ipcRender = require("electron").ipcRenderer;
 
 export default {
   name: "index",
@@ -114,120 +111,124 @@ export default {
   data: function() {
     return {
       items: [],
-      edit_able :{},
-      messageBox:new MessageBox(this),
-      uuid:require('uuid'),
-      keywords:"",
-      icon:"el-icon-view",
-      editingItemKey : null,
-      searchMode :false,
-      canInput : true,
-      canDelete : true,
-      alpha : setting.getAlphaOr(50)/100
+      edit_able: {},
+      messageBox: new MessageBox(this),
+      uuid: require("uuid"),
+      keywords: "",
+      icon: "el-icon-view",
+      editingItemKey: null,
+      searchMode: false,
+      canInput: true,
+      canDelete: true,
+      alpha: setting.getAlphaOr(50) / 100,
+      showSearch: true
     };
   },
   methods: {
     loadData() {
       leveldb.getLatest(5, this.latestKey, data => {
-        if(this.latestKey !== data.key){
-            this.items.push(data);
-            this.latestKey = data.key;
+        if (this.latestKey !== data.key) {
+          this.items.push(data);
+          this.latestKey = data.key;
         }
       });
     },
     loadMore: function() {
-        if(!this.searchMode){
-            this.loadData();
-        }
+      if (!this.searchMode) {
+        this.loadData();
+      }
     },
-    getEmptyItem(){
-        return {
-            "key":"new-one",
-            "value":{
-                "title":"",
-                "content":"",
-                "marked_content":""
-            }
+    getEmptyItem() {
+      return {
+        key: "new-one",
+        value: {
+          title: "",
+          content: "",
+          marked_content: ""
         }
+      };
     },
-    reload(){
+    reload() {
       // 重新加载页面
-      this.items = [] // 清空数据
-      this.latestKey = null //从最新开始
-      this.loadData()
+      this.items = []; // 清空数据
+      this.latestKey = null; //从最新开始
+      this.loadData();
     },
-    editMode(key){
-        this.editingItemKey = key
-        this.$set(this.edit_able,this.editingItemKey,true)
+    editMode(key) {
+      this.editingItemKey = key;
+      this.$set(this.edit_able, this.editingItemKey, true);
     },
-    exitEditMode(){
-        this.$set(this.edit_able,this.editingItemKey,false)
-        this.editingItemKey = null
+    exitEditMode() {
+      this.$set(this.edit_able, this.editingItemKey, false);
+      this.editingItemKey = null;
     },
-    addEmptyCard(){
-        if(this.editingItemKey == null){
-            var item = this.getEmptyItem()
-            this.items.unshift(item)
-            this.editMode(item.key)
-        }
+    addEmptyCard() {
+      if (this.editingItemKey == null) {
+        var item = this.getEmptyItem();
+        this.items.unshift(item);
+        this.editMode(item.key);
+      }
     },
     save(item) {
-        delete item.value.rendered_title
-        if(item.key === "new-one"){ // 如果key为new-one 则表示这是一条需要新增的数据，需要删除key
-            delete item.key
+      delete item.value.rendered_title;
+      if (item.key === "new-one") {
+        // 如果key为new-one 则表示这是一条需要新增的数据，需要删除key
+        delete item.key;
+      }
+      leveldb.put(item, err => {
+        this.messageBox.showMessage(err);
+        if (!err) {
+          this.exitEditMode();
+          this.reload();
         }
-        leveldb.put(item,(err)=>{
-            this.messageBox.showMessage(err)
-            if(!err){
-                this.exitEditMode()
-                this.reload()
-            }
-        }) 
+      });
     },
 
-    cancel(item = null){
-        if(item !== null){ //取消编辑操作
-            if(this.items[0].key === "new-one"){
-                this.items.shift()
-            } 
-            this.exitEditMode()
-        }else if (this.searchMode){ // 退出搜索模式
-            this.searchMode = false
-            this.reload()
-            Bus.$emit("cancel-search")
-            this.icon = "el-icon-view"
+    cancel(item = null) {
+      if (item !== null) {
+        //取消编辑操作
+        if (this.items[0].key === "new-one") {
+          this.items.shift();
         }
+        this.exitEditMode();
+      } else if (this.searchMode) {
+        // 退出搜索模式
+        this.searchMode = false;
+        this.reload();
+        Bus.$emit("cancel-search");
+        this.icon = "el-icon-view";
+      }
     },
-    modify(item){
-        if(this.editingItemKey == null){
-            this.editMode(item.key)
-        }
+    modify(item) {
+      if (this.editingItemKey == null) {
+        this.editMode(item.key);
+      }
     },
-    search(keyword){
-        this.searchMode = true
-        this.items = [] // 清空数据
-        leveldb.search(keyword,(data)=>{
-            this.items.push(data)
-        })
-        this.icon = "el-icon-search"
-        Bus.$emit("on-search") 
+    search(keyword) {
+      this.searchMode = true;
+      this.items = []; // 清空数据
+      leveldb.search(keyword, data => {
+        this.items.push(data);
+      });
+      this.icon = "el-icon-search";
+      Bus.$emit("on-search");
     },
-    onDeleteSuccess(key){
-        leveldb.deleteById(key,(err)=>{
-            this.messageBox.showMessage(err)
-        })
-        this.reload()
+    onDeleteSuccess(key) {
+      leveldb.deleteById(key, err => {
+        this.messageBox.showMessage(err);
+      });
+      this.reload();
     }
   },
   mounted() {
-　　　　let self = this
-       Bus.$on('on-tp-change', (alpha) => {
-            self.alpha = alpha/100.0
-            setting.setAlpha(alpha)
-       })
-       ipcRender.on('cancel',()=>{
-           self.cancel(self.editingItemKey)
-       })
+    let self = this;
+    Bus.$on("on-tp-change", alpha => {
+      self.alpha = alpha / 100.0;
+      setting.setAlpha(alpha);
+    });
+    ipcRender.on("cancel", () => {
+      self.cancel(self.editingItemKey);
+    });
   }
 };
 </script>
