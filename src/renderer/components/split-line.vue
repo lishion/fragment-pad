7671251c1426fb8845b86a4f13752a9132c457b1
lishion <544670411@qq.com>
2019-01-26 19:51:07 +0800

    新增: 分割线组件

---
+++ src/renderer/components/split-line.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="split-line"
+    :style="{backgroundColor:color,height:height,marginTop:top,marginBottom:bottom}"
+  ></div>
+</template>
+
+
+<script>
+export default {
+    name : "splitline",
+    props:{"color":{
+        type:String,
+        default:"rgb(200, 200, 159)"
+    },
+    "top":{
+        default:"12px"
+    },
+    "bottom":{
+        default:"12px"
+    },"height":{
+        default:"1.5px"
+    }}
+}
+</script>
+
