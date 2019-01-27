35d58856d96db06a71215a9cf72985e4c46bbcaa
lishion <544670411@qq.com>
2019-01-27 23:57:57 +0800

    新增: 登录后新增已登录提示

--- src/renderer/components/setting.vue
+++ src/renderer/components/setting.vue
@@ -5,22 +5,24 @@
     </el-select>
     <splitline :color="'rgb(240,235,213)'"></splitline>
     <el-switch
-        v-model="storageModel"
-        active-text="本地模式"
-        inactive-text="远程模式"
-        active-color="#13ce66"
-        inactive-color="#409EFF"
-    >
-    </el-switch>
+      v-model="storageModel"
+      active-text="本地模式"
+      inactive-text="远程模式"
+      active-color="#13ce66"
+      inactive-color="#409EFF"
+    ></el-switch>
     <splitline :color="'rgb(240,235,213)'"></splitline>
     <div class="block">
       <el-slider v-model="initValue" :min="0" :max="100" @change="onTransparencyChange"></el-slider>
     </div>
     <splitline :color="'rgb(240,235,213)'"></splitline>
     <div class="login-input">
-      <el-input v-model="user.username" placeholder="用户名"></el-input>
-      <el-input v-model="user.password" placeholder="密码"></el-input>
-      <el-button size="mini" :style="{width:'100%'}" @click="login">登录</el-button>
+      <div v-if="isLoggedIn"><el-tag type="success">已登录</el-tag></div>
+      <div v-else>
+        <el-input v-model="user.username" placeholder="用户名"></el-input>
+        <el-input v-model="user.password" placeholder="密码"></el-input>
+        <el-button size="mini" :style="{width:'100%'}" @click="login">登录</el-button>
+      </div>
     </div>
     <splitline :color="'rgb(240,235,213)'"></splitline>
     <el-button size="mini" :style="{width:'100%'}" @click="openConsole">控制台</el-button>
@@ -28,14 +30,14 @@
 </template>
 
 <style>
-  .login-input input{
-    margin-top: 3px;
-    margin-bottom:3px; 
-  }
-  .login-input button{
-    margin-top: 3px;
-    margin-bottom:3px; 
-  }
+.login-input input {
+  margin-top: 3px;
+  margin-bottom: 3px;
+}
+.login-input button {
+  margin-top: 3px;
+  margin-bottom: 3px;
+}
 </style>
 
 
@@ -46,12 +48,12 @@ import Bus from "../assets/js/bus";
 import { UserSetting, MessageBox } from "../assets/js/utils";
 import Sender from "../assets/js/sender";
 import { open } from "fs";
-import splitline from "./split-line"
+import splitline from "./split-line";
+import { constants } from "http2";
 const setting = UserSetting.getInstance();
 const ipc = require("electron").ipcRenderer;
 const path = require("path");
 const sender = Sender.getInstance();
-
 
 export default {
   components: { splitline },
@@ -89,7 +91,8 @@ export default {
       initValue: setting.getAlphaOr(0.5),
       user: { username: "", password: "" },
       messageBox: new MessageBox(this),
-      storageModel: true
+      storageModel: true,
+      isLoggedIn: false
     };
   },
   methods: {
@@ -110,7 +113,10 @@ export default {
       sender
         .get("fetch_crsf_token")
         .then(() => sender.post("login", this.user))
-        .then(() => this.messageBox.success("登录成功"))
+        .then(() => {
+          this.messageBox.success("登录成功")
+          this.isLoggedIn = true
+        })
         .catch(message => this.messageBox.failed(message));
     }
   },
@@ -125,6 +131,7 @@ export default {
         ipc.send("on-bg-set");
       }
     });
+    this.isLoggedIn = document.cookie.indexOf("sessionid") !== -1;
   }
 };
 </script>
