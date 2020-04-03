dc6cf8d0be02d88eaf731e54ae6dda3dc2cc5927
linex <544670411@qq.com>
2020-04-03 08:46:12 +0800

    修改: Sender -> Client

--- src/renderer/assets/js/client.js
+++ src/renderer/assets/js/client.js
@@ -11,59 +11,64 @@ Promise.prototype.finally = function (callback) {
 
 import axios from 'axios'
 
-export default class Sender {
+export default class Client {
     constructor(axios) {
         this.axios = axios;
     }
 
-    static getInstance(){
-        const dev = process.env.NODE_ENV === "development"
-        if(!Sender.instance){
+    static getInstance() {
+        const dev = process.env.NODE_ENV === "development";
+        if (!Client.instance) {
             const axiosInstance = axios.create({
-                // baseURL: dev ? "http://127.0.0.1:8000" : "http://www.fern-li.cn",
-                baseURL: "http://148.70.232.85",
-                xsrfCookieName:'csrftoken',
-                xsrfHeaderName:'X-CSRFtoken',
+                baseURL: dev ? "http://127.0.0.1:8000" : "http://www.fern-li.cn",
+                // baseURL: "http://148.70.232.85",
+                xsrfCookieName: 'csrftoken',
+                xsrfHeaderName: 'X-CSRFtoken',
                 withCredentials: true
             });
-            Sender.instance = new Sender(axiosInstance)
+            Client.instance = new Client(axiosInstance)
         }
-        return Sender.instance
+        return Client.instance
     }
+
     get(url) {
         let response = this.axios.get(url);
         return this.handleResponse(response);
     }
+
     post(url, data) {
         let response = this.axios.post(url, data);
         return this.handleResponse(response);
     }
+
     patch(url, data) {
         let response = this.axios.patch(url, data);
         return this.handleResponse(response);
     }
+
     delete(url, data) {
-        let response = this.axios.delete(url, {"data":data});
+        let response = this.axios.delete(url, {"data": data});
         return this.handleResponse(response);
     }
+
     handleResponse(response) {
         return new Promise((resolve, reject) => {
             response.then(response => {
-                    if (response.data.status != "success") {
-                        reject(response.data.message);
-                    } else {
-                        resolve(response.data.data);
-                    }
-                })
+                if (response.data.status !== "success") {
+                    reject(response.data.message);
+                } else {
+                    resolve(response.data.data);
+                }
+            })
                 .catch(e => {
-                    if(e.response){
-                        const data = e.response.data
-                        if(data && "message" in data){
+                    if (e.response) {
+                        const data = e.response.data;
+                        if (data && "message" in data) {
                             reject(data.message)
-                        }else{
+                        } else {
                             reject("网络错误")
                         }
-                    }else{
+                    } else {
                         reject("网络错误")
                     }
                 });
@@ -72,6 +77,6 @@ export default class Sender {
 }
 
 
-Sender.instance = null
+Client.instance = null;
  
 
